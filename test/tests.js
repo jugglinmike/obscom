d0716ea0855e088be82af12a64556612337391c4
Mike Pennisi <mike@mikepennisi.com>
2013-02-02 20:27:29 -0500

    Generalize

--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,10 @@
-var seinbot = require("../seinbot");
+var obscom = require("../obscom");
 var data = require("./data.json");
 
 data.forEach(function(testData, idx) {
 
 	exports["test" + idx] = function(test) {
-		var actual = seinbot(testData.input);
+		var actual = obscom(testData.input);
 		test.deepEqual(testData.expected, actual);
 		test.done();
 	};
